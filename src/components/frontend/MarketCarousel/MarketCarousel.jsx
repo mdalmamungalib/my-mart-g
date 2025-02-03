19c0d175d23a8e23a70c1cf2483efd73bf5d3079
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-02-03 09:22:26 +0600

    market carousel add

--- src/components/frontend/MarketCarousel/MarketCarousel.jsx
+++ src/components/frontend/MarketCarousel/MarketCarousel.jsx
@@ -7,24 +7,97 @@ import "react-multi-carousel/lib/styles.css";
 
 const MarketCarousel = () => {
   const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 6,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
+      items: 2,
     },
   };
+
+  const marketList = [
+    {
+      id: 1,
+      name: "Apple",
+      image:
+        "https://img.freepik.com/free-psd/close-up-apples-isolated_23-2151598206.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid",
+      description:
+        "Authentic Indian masalas to spice up your meals.",
+    },
+    {
+      id: 2,
+      name: "Tomato",
+      image:
+        "https://img.freepik.com/free-photo/tomatoes_144627-15413.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid",
+      description:
+        "Crunchy and delicious snacks for every occasion.",
+    },
+    {
+      id: 3,
+      name: "Lemon",
+      image:
+        "https://img.freepik.com/free-vector/citrus-fruits-collection-realistic-composition_1284-15784.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid",
+      description: "Refreshing beverages for a healthy lifestyle.",
+    },
+    {
+      id: 4,
+      name: "Anaros",
+      image:
+        "https://img.freepik.com/free-photo/colorful-fruits-tasty-fresh-ripe-juicy-white-desk_179666-169.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid",
+      description:
+        "Daily essentials and groceries at the best prices.",
+    },
+    {
+      id: 5,
+      name: "Fruits & Vegetables",
+      image:
+        "https://img.freepik.com/free-photo/creative-composition-with-fruits-texture-vibrant-colors_23-2149888001.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid",
+      description: "Fresh and organic fruits and vegetables.",
+    },
+    {
+      id: 6,
+      name: "Bakery",
+      image:
+        "https://media.istockphoto.com/id/465015726/photo/dabba-masala.jpg?s=612x612&w=0&k=20&c=QbZ2KHUJpVTo4eLkzELdyE9YrLU1X0eDDoAs290g7_k=",
+      description: "Freshly baked bread and bakery items.",
+    },
+    {
+      id: 7,
+      name: "Dairy Products",
+      image:
+        "https://media.istockphoto.com/id/465015726/photo/dabba-masala.jpg?s=612x612&w=0&k=20&c=QbZ2KHUJpVTo4eLkzELdyE9YrLU1X0eDDoAs290g7_k=",
+      description:
+        "High-quality dairy products for your daily needs.",
+    },
+    {
+      id: 8,
+      name: "Frozen Foods",
+      image:
+        "https://media.istockphoto.com/id/465015726/photo/dabba-masala.jpg?s=612x612&w=0&k=20&c=QbZ2KHUJpVTo4eLkzELdyE9YrLU1X0eDDoAs290g7_k=",
+      description: "Quick and easy frozen foods for busy days.",
+    },
+    {
+      id: 9,
+      name: "Spices",
+      image:
+        "https://media.istockphoto.com/id/465015726/photo/dabba-masala.jpg?s=612x612&w=0&k=20&c=QbZ2KHUJpVTo4eLkzELdyE9YrLU1X0eDDoAs290g7_k=",
+      description: "Exotic spices to enhance your cooking.",
+    },
+    {
+      id: 10,
+      name: "Personal Care",
+      image:
+        "https://media.istockphoto.com/id/465015726/photo/dabba-masala.jpg?s=612x612&w=0&k=20&c=QbZ2KHUJpVTo4eLkzELdyE9YrLU1X0eDDoAs290g7_k=",
+      description:
+        "Skincare, haircare, and personal hygiene products.",
+    },
+  ];
   return (
     <Carousel
       swipeable={false}
@@ -43,48 +116,24 @@ const MarketCarousel = () => {
       removeArrowOnDeviceType={["tablet", "mobile"]}
       // deviceType={this.props.deviceType}
       dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
+      itemClass="px-2"
     >
-      <Link href="/" className="rounded-lg">
-        <Image
-          width={556}
-          height={556}
-          className="object-cover w-full"
-          src="https://img.freepik.com/free-photo/spices-spoons-wooden-table_176420-111.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid"
-          alt="Market"
-        />
-        <h2 className="bg-slate-600 text-slate-300">Vegetables</h2>
-      </Link>
-      <Link href="/" className="rounded-lg">
-        <Image
-          width={556}
-          height={556}
-          className="object-cover w-full"
-          src="https://img.freepik.com/free-photo/spices-spoons-wooden-table_176420-111.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid"
-          alt="Market"
-        />
-        <h2 className="bg-slate-600 text-slate-300">Vegetables</h2>
-      </Link>
-      <Link href="/" className="rounded-lg">
-        <Image
-          width={556}
-          height={556}
-          className="object-cover w-full"
-          src="https://img.freepik.com/free-photo/spices-spoons-wooden-table_176420-111.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid"
-          alt="Market"
-        />
-        <h2 className="bg-slate-600 text-slate-300">Vegetables</h2>
-      </Link>
-      <Link href="/" className="rounded-lg">
-        <Image
-          width={556}
-          height={556}
-          className="object-cover w-full"
-          src="https://img.freepik.com/free-photo/spices-spoons-wooden-table_176420-111.jpg?ga=GA1.1.518786193.1722775559&semt=ais_hybrid"
-          alt="Market"
-        />
-        <h2 className="bg-slate-600 text-slate-300">Vegetables</h2>
-      </Link>
+      {marketList.map((market) => {
+        return (
+          <Link key={market.id} href="/" className="mr-2 bg-red-600 rounded-lg">
+            <Image
+              width={556}
+              height={556}
+              className="object-cover w-full rounded-2xl max-h-36"
+              src={market.image}
+              alt="Market"
+            />
+            <h2 className="text-center ">
+              {market.name}
+            </h2>
+          </Link>
+        );
+      })}
     </Carousel>
   );
 };
--- src/components/frontend/MarketList/MarketList.jsx
+++ src/components/frontend/MarketList/MarketList.jsx
@@ -4,8 +4,11 @@ import MarketCarousel from "../MarketCarousel/MarketCarousel";
 const MarketList = () => {
   return (
     <div>
-      <h1>Market List</h1>
+      
+      <div className="p-4 rounded-lg bg-slate-100 dark:bg-lime-900">
+      <h1 className="py-2 mb-4 text-2xl font-semibold text-center dark:text-slate-50 text-slate-900">Sop By Market</h1>
       <MarketCarousel />
+      </div>
     </div>
   );
 };
